633f4350d2b94b5891e8b6c1cb7bc1bb218946ce
Aman Rajput <ar3698892@gmail.com>
2025-08-10 14:07:02 +0530

    commit

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ const PORT = process.env.PORT || 8080;
 
 
 
-app.use(cors({
-  origin: 'http://localhost:5173',  
-  credentials: true                 
-}));
+
 //body parser
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
